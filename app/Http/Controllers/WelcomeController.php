7610ee1799b0b86a3ca90cb7cab1edac75113258
Zalbaq <ihza.nk.160903@gmail.com>
2024-02-28 16:25:06 +0700

    [Add] blog views and [Add][function] WelcomeController

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,4 +10,11 @@ class WelcomeController extends Controller
     {
         return 'Hello World';
     }
+
+    public function greeting()
+    {
+        return view('blog.hello')
+        ->with('name', 'Andi')
+        ->with('occupation', 'Astronout');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -86,3 +86,11 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+
+// PRAKTIKUM VIEWS
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Andi']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
